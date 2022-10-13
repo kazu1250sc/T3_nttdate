2051ca45bb1a1f697548503fa6e6e4274d350022
KotonoSugi <115690440+KotonoSugi@users.noreply.github.com>
2022-10-13 15:54:19 +0900

    Add files via upload

---
+++ matching_image/src/main/java/com/example/demo/MatchingImageApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchingImageApplication.class, args);
+	}
+
+}
