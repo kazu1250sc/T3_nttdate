08de46124205c1d7db1f55a44ad9e69ea883fc5f
和田優香 <@DESKTOP-ESUT3DR>
2022-10-17 16:09:08 +0900

    サンプルです

---
+++ springSample/src/main/java/com/example/demo/service/ModeSearchService.java
@@ -0,0 +1,19 @@
+/*package com,exammple,demo,service;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+@Service
+public class ModeSerchService{
+	
+	@Autowired
+	private ModeSerchRepository modeSerchRepository;
+	
+	public List<String> {
+		return modeSerchService.findAll();
+	}
+}*/
